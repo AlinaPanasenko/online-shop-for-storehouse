8d41eeaa7d8f3d8ec2818241ff6bc91d03725a5e
AlinaPanasenko <maikshev@gmail.com>
2023-01-27 21:11:01 +0000

    Add menu function that provide user with options

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ Imported libraries and constant variables were written as in the Love Sandwiches
 
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,6 +20,50 @@ SHEET = GSPREAD_CLIENT.open('online_shop_for_storehouse')
 
 orders = SHEET.worksheet('orders')
 
-data = orders.get_all_values()
+prices = SHEET.worksheet('prices')
 
-print(data)
+
+def menu():
+
+    """
+    Provides the customer with a list of products 
+    with prices and a specific order index
+    """
+
+    print('Welcome to automatic order system!')
+    print('What would you like to order?')
+
+    column_prices = prices.col_values(3)
+    column_product = prices.col_values(2)
+    column_index = prices.col_values(1)
+
+    list_for_customer = list(zip(column_index, column_product, column_prices))
+    print(tabulate(list_for_customer))
+
+    return list_for_customer
+
+
+
+
+def get_order():
+    print('Please, select from list below and type index to the console.')
+    print('For example, A1')
+
+    data_input = input('Enter your order here: \n')
+
+print('Please, make your order')
+print('Provide index of the product and quantity, separated by coma')
+print('Fpr example: 1,60\n')
+
+data_input = input('Enter your order here: \n')
+order_data = data_input.split(',')
+order_index = order_data[0]
+order_product = order_data[1]
+
+
+print(order_index)
+print(order_product)
+print(order_price)
+
+
+menu()
